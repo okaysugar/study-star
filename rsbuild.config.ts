d64bc403dc838dfae75870705a0ccfb037411b56
chrisbob.cai <chrisbob.cai@meetsocail.com>
2026-03-20 14:15:15 +0800

    update

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,20 +1,15 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-
-const repoName = 'study-star';
-const assetPrefix = process.env.NODE_ENV === 'production' ? `/${repoName}/` : '/';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
-  plugins: [pluginReact()],
-  html: {
-    title: 'Study Star ⭐',
-    meta: {
-      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
-      'apple-mobile-web-app-capable': 'yes',
-      'apple-mobile-web-app-status-bar-style': 'default',
-    },
-  },
-  output: {
-    assetPrefix,
-  },
+	plugins: [pluginReact()],
+	html: {
+		title: "Study Star ⭐",
+		meta: {
+			viewport:
+				"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+			"apple-mobile-web-app-capable": "yes",
+			"apple-mobile-web-app-status-bar-style": "default",
+		},
+	},
 });
